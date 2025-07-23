b0a8ef0b60b8b3c613d59542400c451de5bacf10
Bboyfinger <tuvngdevsl@gmail.com>
2025-07-23 15:37:31 +0700

    update

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+axios.defaults.withCredentials = true;
+
 const axiosInstance = axios.create({
   baseURL: 'https://hagiangtravel.onrender.com/api',
   // http://localhost:5000/api
