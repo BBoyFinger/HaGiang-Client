34e8056792d5958205367bdda53b4af50b6ba6cd
Bboyfinger <tuvngdevsl@gmail.com>
2025-07-23 11:26:40 +0700

    update fe

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -3,6 +3,9 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   baseURL: 'https://hagiangtravel.onrender.com/api',
   // http://localhost:5000/api
+  headers: {
+    "Content-Type": "application/json",
+  },
   withCredentials: true, // Enable sending cookies
   // Có thể thêm các cấu hình khác như headers, timeout ở đây nếu cần
 });
